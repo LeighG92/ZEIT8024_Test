6a8f2fa8876530c28c150819ee7f3ac143d094b7
LeighG92 <92023946+LeighG92@users.noreply.github.com>
2023-06-11 11:08:47 +0100

    Update and rename Hello.java to Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
-Public class Hello {
-  public static void main (String[] args) {
+public class Test {
+  public static void main(String[] args) {
     //displays "This is a test for a Zeit8024 exercise" in the console
     System.out.println("This is a test for a Zeit8024 exercise");
   }
