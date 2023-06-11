d3359d0c8c3fc3fa0c3e51fd9b931c4b83fe4600
LeighG92 <92023946+LeighG92@users.noreply.github.com>
2023-06-11 12:05:27 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
   public static void main(String[] args) {
-    //displays "This is a test for a Zeit8024 exercise" in the console
-    System.out.println("This is a test for a Zeit8024 exercise");
+    //displays "This is a test for a Zeit8024 exercise, following a repository commit, to check if the Jenkins pipeline automatically runs" in the console
+    System.out.println("This is a test for a Zeit8024 exercise, following a repository commit, to check if the Jenkins pipeline automatically runs");
   }
 }
